0c6c0b5b1c687eda83706be5ba09cb399341e6e9
Amar2050 <amarboutayeb@gmail.com>
2023-04-27 12:05:47 +0200

    TP ALT Correction

--- partials/car.card.php
+++ partials/car.card.php
@@ -1,21 +1,44 @@
 <div class="card text-center shadow my-3">
+
     <div class="card-header bg-dark text-white ">
-        <h2 class="card-title"><?= $car["name"] ?></h2>
+        <h2 class="card-title"><?= ucwords($car["name"]) ?></h2>
     </div>
 
     <div class="card-body">
         <img src="<?= $car["coverImage"] ?>" class="img-fluid" alt="photo de <?= $car["name"] ?>">
         <hr>
-        <p>Origine: <?= $car["pays"] ?>
-
+        <p>Origine:
+            <?php if ($car["pays"]) : ?>
+                <span><?= mb_strtoupper($car["pays"]) ?></span>
+            <?php else : ?>
+                <span>NC</span>
+            <?php endif; ?>
         </p>
 
-        <p>Puissance: <span class="badge badge-info"><?= $car["power"] ?> CH</span></p>
+        <p>Puissance:
+                                <!-- SWICTCH  -->
+<?php switch ($car["power"]): ?>
+<?php case $car["power"] < 1000: ?>
+    <span class="badge badge-danger"> <?= $car["power"] ?> CH</span>
+<?php break; ?>
 
-        <p>0 à 100 km/h: <span> <?= $car["perf"] ?> sec</span></p>
+<?php case $car["power"] >= 1000 && $car["power"] <= 1200 : ?>
+    <span class="badge badge-warning"> <?= $car["power"] ?> CH</span>
+<?php break; ?>
+
+<?php case $car["power"] > 1200: ?>
+    <span class="badge badge-success"> <?= $car["power"] ?> CH</span>
+<?php break; ?>
+
+<?php endswitch; ?>
+                            <!-- END SWICTCH  -->
+        </p>
+
+
+
+        <p>0 à 100 km/h: <span><?= $car["perf"] ?> sec</span></p>
 
         <p>Réserver maintenant !</p>
-
         <button class="btn btn-primary ">Réserver</button>
     </div>
 </div>
--- partials/driver.card.php
+++ partials/driver.card.php
@@ -1,6 +1,10 @@
-<div class="card text-center shadow my-3">
-    <div class="card-header bg-dark text-white ">
-        <h2 class="card-title"><?= $driver["fullName"] ?></h2>
+<div class="card text-center shadow my-3 
+    <?= ($driver["likeIts"] > 0) ? "border border-success" : ( ($driver["likeIts"] < 0) ? "border border-danger" : "") ?>">
+<!-- ternaire imbriqué SI SINON (SI SINON)  Si positif vert si négatif rouge et sinon neutre (0) donc rien -->
+    <div class="card-header bg-dark text-white">
+        <h2 class="card-title">
+            <?= ucwords($driver["fullName"] )?>
+        </h2>
     </div>
 
     <div class="card-body">
@@ -8,11 +12,44 @@
         <img src="<?= $driver["coverImage"] ?>" width="300px" height="150px" class="img-fluid" alt="photo <?= $driver["fullName"] ?>">
         <hr>
 
-        <p>Origine: <?= $driver["pays"] ?></p>
+        <p>Origine:
+            <?php if ($driver["pays"]) : ?>
+                <span><?= mb_strtoupper($driver["pays"]) ?></span>
+            <?php else : ?>
+                <span>NC</span>
+            <?php endif; ?>
+        </p>
 
         <p>Discipline:
-            <span class="badge badge-success"><?= $driver["category"] ?></span>
+
+                                        <!-- SWICTCH  -->
+<?php switch ($driver["category"]): ?>
+<?php case $driver["category"] == "formule 1": ?>
+    <span class="badge badge-success"> <?= $driver["category"] ?> </span>
+<?php break; ?>
+
+<?php case $driver["category"] == "gymkhana": ?>
+    <span class="badge badge-primary"> <?= $driver["category"] ?> </span>
+<?php break; ?>
+
+<?php case $driver["category"] == "drag": ?>
+    <span class="badge badge-info"> <?= $driver["category"] ?> </span>
+<?php break; ?>
+
+<?php case $driver["category"] == "rallye": ?>
+    <span class="badge badge-danger"> <?= $driver["category"] ?> </span>
+<?php break; ?>
+
+<?php case $driver["category"] == "nascar": ?>
+    <span class="badge badge-warning"> <?= $driver["category"] ?> </span>
+<?php break; ?>
+
+<?php endswitch; ?>
+                            <!-- END SWICTCH  -->
+         
         </p>
+
+
 
         <button class="btn btn-success mx-2 border">
             <i class="fas fa-thumbs-up"></i>
@@ -21,6 +58,7 @@
         <button class="btn btn-danger border">
             <i class="fas fa-thumbs-down"></i>
         </button>
+
     </div>
 </div>
 <hr>
